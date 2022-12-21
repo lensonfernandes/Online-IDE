a7373afdddd9982d12504df5a100b19366b9b0ff
lensonfernandes <lenson.fernand@gmail.com>
2022-12-21 12:20:30 +0530

    Right Pane skeletal content Added

---
+++ onlineide/src/Components/Home/PlaygroundCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function PlaygroundCard() {
+  return (
+    <div>
+        <img></img>
+        <div className="info">
+            <h5>Info Title</h5>
+            <h6>Language: JavaScript</h6>
+        </div>
+        <img></img>
+        <img></img>
+    </div>
+  )
+}
--- onlineide/src/Components/Home/RightPaneHomeScreen.jsx
+++ onlineide/src/Components/Home/RightPaneHomeScreen.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import PlaygroundCard from './PlaygroundCard'
 
 function RightPaneHomeScreen() {
   return (
-    <div>RightPaneHomeScreen</div>
+    <div>
+        <div>
+            <h3>My Playground</h3>
+            <h4>+ New Folder</h4>
+        </div>
+        <hr/>
+        <div>
+            <div className="left">
+                 <img></img>
+                <h4> My Folder</h4>
+            </div>
+
+            <div className="right">
+                 <img></img>
+                 <img></img>
+                <h5>+ New Playground</h5>
+            </div>
+           
+
+           
+        </div>
+        <hr/>
+
+        <PlaygroundCard />
+        <PlaygroundCard />
+        <PlaygroundCard />
+        <PlaygroundCard />
+    </div>
   )
 }
 
