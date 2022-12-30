c42a4494cc36685d35aed8c9f621ab82f6f9cb5b
lensonfernandes <lenson.fernand@gmail.com>
2022-12-30 18:07:34 +0530

    New Playground Button Functionality Added

--- onlineide/src/Components/ModalTypes/NewPlayGround.jsx
+++ onlineide/src/Components/ModalTypes/NewPlayGround.jsx
@@ -8,19 +8,19 @@ function NewPlayGround() {
 
 const {isOpenModal, closeModal} = React.useContext(ModalContext);
 const {addPlayground} = React.useContext(PlaygroundContext);
-const {folderId} = isOpenModal.identiifers;
+const {folderId} = isOpenModal.identifiers;
 const [playgroundName, setPlayGroundName] = React.useState("");
 
 
   return (
-    <div>
+    <div className='px-6 py-4 mb-8 flex flex-col items-center justify-center gap-6'>
       <h2>Create New Playground</h2>
       <input type="text" value={playgroundName} onChange={e=>setPlayGroundName(e.target.value)}
-      ></input>
+      className='border-[.5px] border-black  p-3 font-semibold w-full rounded-lg shadow-lg'></input>
       <button onClick={()=>{
         addPlayground(folderId, playgroundName, "javascript")
         closeModal()
-      }}>
+      }} className='p-4 w-fit text-black bh-white rounded-lg border-[5px] border-darkBlue shadow-lg '>
         Create
       </button>
     </div>
